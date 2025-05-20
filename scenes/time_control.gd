28a056e1181433a120be32bc9e61f0dde42d5a6a
attidaddy <atticusauerbach@gmail.com>
2025-05-20 17:15:30 +0200

    time speed

--- scenes/time_control.gd
+++ scenes/time_control.gd
@@ -6,19 +6,19 @@ func _update_buttons(active_button: Button):
 	active_button.modulate = Color(0.5, 0.5, 0.5, 1) # Darker color to show active state
 	active_button.release_focus()
 
-func _on_pause_button_pressed():
+func _on_pause_pressed():
 	GameClock.pause()
 	_update_buttons($Pause)
 
-func _on_play_button_pressed():
+func _on_play_pressed():
 	GameClock.play()
 	_update_buttons($Play)
 
-func _on_fast_button_pressed():
+func _on_fast_pressed():
 	GameClock.fast()
 	_update_buttons($Fast)
 
-func _on_super_fast_button_pressed():
+func _on_super_fast_pressed():
 	GameClock.super_fast()
 	_update_buttons($SuperFast)
 
@@ -27,10 +27,10 @@ func _input(event):
 		return
 
 	if event.is_action_pressed("Pause"):
-		_on_pause_button_pressed()
+		_on_pause_pressed()
 	elif event.is_action_pressed("Play"):
-		_on_play_button_pressed()
+		_on_play_pressed()
 	elif event.is_action_pressed("Fast"):
-		_on_fast_button_pressed()
+		_on_fast_pressed()
 	elif event.is_action_pressed("SuperFast"):
-		_on_super_fast_button_pressed()
+		_on_super_fast_pressed()
